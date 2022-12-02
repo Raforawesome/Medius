e49949361506a6377ab788407ea835d2aaf4332f
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-12-02 13:16:12 -0500

    Add directory picker component

--- src/components/dir_select.rs
+++ src/components/dir_select.rs
@@ -1,5 +1,33 @@
 use dioxus::prelude::*;
 
 pub fn default(cx: Scope) -> Element {
-    rsx!(cx, h1 {})
+    let selected = use_state(&cx, || "".to_string());
+    rsx!(cx,
+        style { [include_str!("../assets/css/dir_select.css")] }
+        p {
+            class: "select-box",
+            [if selected.get() != "" {
+                selected.get()
+            } else {
+                "Select a directory"
+            }]
+        }
+        button {
+            class: "select-button",
+            onclick: |_| {
+                let res = rfd::FileDialog::new()
+                    .set_directory(dirs::home_dir().unwrap())
+                    .pick_folder();
+                if let Some(p) = res {
+                    selected.modify(|_| p.to_str().unwrap().to_string());
+                }
+            },
+            "Select"
+        }
+        button {
+            class: "go-button",
+            onclick: |_| {},
+            "Go!"
+        }
+    )
 }
